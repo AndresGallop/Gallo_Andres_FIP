46a5f4c358f5f03b2ca535af9be38836bb39d028
Andrés Gallo <andresgallod.i@gmail.com>
2020-08-06 01:47:10 -0400

    already done in design

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
-/*(() => {
+(() => {
 	// "use strict";
 	console.log("fired");
 
 	let button = document.querySelector("#button"),
-			burgerCon = document.querySelector("#burgerCon");
+			burgerCon = document.querySelector("#burgerCon"),
+      selector = document.querySelectorAll(".seemento"),
+      proms = document.querySelector("#ads")
 
-	function hamburgerMenu() {
-		burgerCon.classList.toggle("slideToggle");
-		button.classList.toggle("expanded");
-	}
+	//function hamburgerMenu() {
+	//	burgerCon.classList.toggle("slideToggle");
+	//	button.classList.toggle("expanded");
+	//}
 
-	button.addEventListener("click", hamburgerMenu);
+  function animateBanner() {
+
+    console.log("works");
+
+    proms.style.right = `${this.dataset.offset* 400}px`;
+
+		debugger;
+
+    //and also show the house name
+  //  houseName.textContent = `House ${houseInfo[this.dataset.offset][0]}`;
+  //  houseBio.textContent = `${houseInfo[this.dataset.offset][1]}`;
+    //debugger;
+  }
+
+  selector.forEach(sel => sel.addEventListener("onclick", animateBanner));
+	//button.addEventListener("click", hamburgerMenu);
 })();
-*/
