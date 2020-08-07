07b8b43c9cf28647437cb0794dd884338b593791
Andrés Gallo <andresgallod.i@gmail.com>
2020-08-07 03:27:02 -0400

    burger menu done

--- js/main.js
+++ js/main.js
@@ -2,30 +2,36 @@
 	// "use strict";
 	console.log("fired");
 
-	let button = document.querySelector("#button"),
+	let button = document.querySelector("#hamenu"),
 			burgerCon = document.querySelector("#burgerCon"),
       selector = document.querySelectorAll(".seemento"),
       proms = document.querySelector("#ads");
 
-	//function hamburgerMenu() {
-	//	burgerCon.classList.toggle("slideToggle");
-	//	button.classList.toggle("expanded");
-	//}
+	function hamburgerMenu() {
+		burgerCon.classList.toggle("slideToggle");
+		button.classList.toggle("expanded");
+	}
 
   function animateBanner() {
 
     console.log("works");
 
     proms.style.right = `${this.dataset.offset* 400}px`;
-
-
-
     //and also show the house name
   //  houseName.textContent = `House ${houseInfo[this.dataset.offset][0]}`;
   //  houseBio.textContent = `${houseInfo[this.dataset.offset][1]}`;
     //debugger;
   }
 
+	/*function blurer(){
+
+			document.getElementById("blur").style.filter = "blur(8px)";
+	}*/
+
+
+
   selector.forEach(sel => sel.addEventListener("click", animateBanner));
+	button.addEventListener("click", hamburgerMenu);
+	//button.addEventListener("click", blurer);
 	//button.addEventListener("click", hamburgerMenu);
 })();
